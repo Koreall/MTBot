7b805a2ae5283034c4eb998909c29f0783c46e87
Koreall <en.petit@laposte.net>
2020-05-07 14:53:44 +0200

    Added config file, total credits

--- Minitrooper bot.py
+++ Minitrooper bot.py
@@ -1,8 +1,9 @@
 import requests as rq
 import time
+import re
 
 def connect(s,name, passwd):
-	r = s.post("http://{}.minitroopers.fr/login".format(name), data=({'login':name, 'pass':passwd} if passwd else {"login":name})) #Truthfully, name doesn't have to be in url
+	r = s.post("http://{}.minitroopers.fr/login".format(name.replace(' ','-')), data=({'login':name, 'pass':passwd} if passwd else {"login":name})) #Truthfully, name doesn't have to be in url
 	if "correct" in r.history[0].cookies["ssid"]:
 		return None
 	return r
@@ -17,12 +18,16 @@ def playAllAttacks(s, r, chk, ennemy=None):
 	r = s.get(r.url[:-3]+"/b/opp")
 	for i in range(3):
 		print("\tAttack n°{}: Pending...".format(i), end='\r', flush=True)
-		T = s.cookies["ssid"]
-		start = T.find("oppsli") #See the keepnote book. Attackable opponents are in a list in the cookie
-		end = T.find("i", start+6)
-		ennemy = T[start+6:end]
-		attackurl = r.url[:-3]+"battle?opp={};chk={}".format(ennemy, chk)
-		a = s.get(attackurl)
+		if ennemy:
+			attackurl = r.url[:-3] + "battle"
+			a = s.post(attackurl, data={"friend":ennemy,"chk":chk})
+		else:
+			T = s.cookies["ssid"]
+			start = T.find("oppsli") #See the keepnote book. Attackable opponents are in a list in the cookie
+			end = T.find("i", start+6)
+			opp = T[start+6:end]
+			attackurl = r.url[:-3]+"battle?opp={};chk={}".format(opp, chk)
+			a = s.get(attackurl)
 		time.sleep(1)
 		results.append("remport" in a.history[0].cookies["ssid"])
 		print("\tAttack n°{}: {}".format(i, "Victory!  " if results[-1] else "Defeat.   "), flush=True)
@@ -34,6 +39,9 @@ def isMissionUnlocked(r):
 def isRaidUnlocked(r):
 	start = r.content.find(b'/b/raid')
 	return start != -1
+
+def unlockMission(s,r,chk):
+	s.get(r.url[:-2]+"unlock?mode=miss;chk={}".format(chk))
 
 def playMission(s, r, chk):
 	print("Mission:")
@@ -54,50 +62,104 @@ def playRaid(s, r, chk):
 	attackurl = r.url[:-3]+"/b/raid?chk={}".format(chk)
 	stage = 0
 	while True:
-		a = s.get(attackurl, allow_redirects=False)
+		a = s.get(attackurl)
 		stage += 1
 		print("\tStage: {}. Raiding...".format(stage), flush=True, end='\r')
 		time.sleep(1)
-		if "remport" not in a.headers["Set-Cookie"]:
+		if "remport" not in a.history[0].cookies["ssid"]:
 			break
 	print("\tStage: {}. Raid has ended.".format(stage))
 	return 4*stage
 
-def farmPlayer(name, passwd):
+def getCredits(r):
+	start = r.content.find(b'class="money"')
+	start = r.content.find(b"\n", start)
+	end = r.content.find(b"\n", start+1)
+	money = int(r.content[start+1:end])
+	return money
+
+def farmPlayer(name, passwd,D):
 	s = rq.Session()
-	money = 0
-	print("Player: " + name, flush = True)
+	gain = 0
+	print("--------\nPlayer: " + name, flush = True)
 	r = connect(s, name, passwd)
 	if not r:
 		print("Incorrect playername or password: {}, {}".format(name, passwd))
 		return
 	chk = getChk(r)
-	money += playAllAttacks(s, r, chk)
+	credits = getCredits(r)
+	gain += playAllAttacks(s, r, chk, D["ennemy"]) if "ennemy" in D else playAllAttacks(s, r, chk)
 	print() #Get to the next line
 	if isMissionUnlocked(r): 
-		money += playMission(s, r, chk)
+		gain += playMission(s, r, chk)
 	else: 
 		print("Mission: Not unlocked.",end = '')
+		if "buyMission" in D and D["buyMission"].lower() == "true" and credits+gain >= 5:
+			unlockMission(s,r, chk)
+			print("\nMissions were unlocked. (buyMission config)")
+			gain += -5
+			gain += playMission(s,r,chk)
 	print()
 	if isRaidUnlocked(r): 
-		money += playRaid(s, r, chk)
+		gain += playRaid(s, r, chk)
 	else: 
 		print("Raid: Not unlocked.")
-	print("\n{} total gain: {} credits.\n\n".format(name,money))
+	print("\n{} total gain: {} credits (=> {} total).\n\n".format(name,gain,credits+gain))
 
 
 def farmList(loc):
 	Players=[]
+	D = readconfigfile()
 	try:
 		with open("mtbotlist.txt",'r') as f:
+			print("[+] Bot list found.")
 			for line in f:
 				if line[0] == '#':
 					continue
 				start = line.find(":")
 				line = line.strip()
 				Players.append((line[:start],line[start+1:]) if start != -1 else (line,""))
-	except Exception as e:
-		raise e
+			print("[+] Bot list read. ({} accounts read)".format(len(Players)) if len(Players)>0 else "[!] Bot list empty. Please add your accounts to the list")
+	except:
+		print("[!] Bot list not found.")
+		try:
+			with open("mtbotlist.txt",'w') as f:
+				print("[+] Created default bot list (mtbotlist.txt). Please add your accounts to the list.")
+		except:
+			print("[!] Could not create file, please add it manually or grant rights to this bot")
 
 	for n, p in Players:
-		farmPlayer(n,p)
+		farmPlayer(n,p,D)
+
+	print("[END] Bot ended sucessfully.")
+
+
+def readconfigfile():
+	D = {}
+	try:
+		with open("config.txt",'r') as f:
+			for line in f:
+				if line[0] == '#':
+					continue
+				line = line.strip()
+				m = re.match(r"^(\w*)=(.*)$", line)
+				if m :
+					D[m.group(1)] = m.group(2)
+	except:
+		print("[!] Config file not found. Assuming default parameters.")
+		try:
+			with open("config.txt",'w') as f:
+				f.write("#Default config file, change it for your needs\n\n")
+				f.write("#write a name to attack the same ennemy each time, or leave it blanc to choose randomly\n")
+				f.write("ennemy=\n\n")
+				f.write("#if this option is true, it checks if it can buy the access to Missions (which is worth over time)\n")
+				f.write("buyMission=False\n\n")
+				f.write("#Select the log level (1 = minimal, 2 = full)\n")
+				f.write("logLevel=2")
+		except Exception as e:
+			print("[!!!] Could not create a config file. Error: "+e)
+		else:
+			print("[+] A config file with default parameters has been created.")
+	else:
+		print("[+] Parameters read from config file.")
+	return D
--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # MTbot
 
-Small botting program made for [Minitroopers](http://minitroopers.fr), all rights reserved.
+Small botting program made for [Minitroopers](http://enzoo987.minitroopers.fr), all rights reserved.
 Easy of use, just put your accounts in the mtbotlist.txt file and run the python script, which will farm them.
+Any idea for improvement is welcome, just open an issue to submit your idea.
+
+### Functionalities
+
+* Farms battles, missions and raids for any account.
+* Farms a whole list of account with their passwords.
+* Autobuy missions if affordable.
+* Reports the results of each action.
+* Possibility to select an opponent and have every account attack him in battle (effectively raiding him).
+[ ] Tells you if a trooper can be upgraded or if you can afford a new one.
+[ ] Different log levels, to prevent your console from getting flooded.
+[ ] A beautiful GUI for immensly improved usability. (well, in a distant future maybe)
 
 ### Requirements
 
-The script requires the [requests](https://2.python-requests.org/en/master/) python library as well as python3 to work (tested on 3.6).
+The script requires the [requests](https://2.python-requests.org/en/master/) python library as well as python3 to work (built on 3.6).
+It needs rights to read the files in its directory.
+
+### Warnings
+
+The script was never tested on an account which already had actions performed for the day. Please only use clean accounts.
